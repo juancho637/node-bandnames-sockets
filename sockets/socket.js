3b84841190c081526f7785428baffcc060bd1845
Juancho Garcia <juancho637@gmail.com>
2020-11-24 12:20:34 -0500

    finalización de los sockets para el módulo de bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,11 +1,32 @@
 const { io } = require("../index");
+const Band = require("../models/band");
+const Bands = require("../models/bands");
+
+const bands = new Bands();
+
+bands.addBand(new Band("banda1"));
+bands.addBand(new Band("banda2"));
+bands.addBand(new Band("banda3"));
+bands.addBand(new Band("banda4"));
 
 io.on("connection", (client) => {
   console.log("Cliente conectado");
 
-  client.on("mensaje", (payload) => {
-    console.log(payload);
-    io.emit("mensaje", { admin: "nuevo mensaje" });
+  client.emit("active-bands", bands.getBands());
+
+  client.on("vote-band", (band) => {
+    bands.voteBand(band.id);
+    io.emit("active-bands", bands.getBands());
+  });
+
+  client.on("add-band", (band) => {
+    bands.addBand(new Band(band.name));
+    io.emit("active-bands", bands.getBands());
+  });
+
+  client.on("delete-band", (band) => {
+    bands.deleteBand(band.id);
+    io.emit("active-bands", bands.getBands());
   });
 
   client.on("disconnect", () => {
