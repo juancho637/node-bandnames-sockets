93be2dbc0c4a5d84b1ac65b969f1102346d487a1
Juancho Garcia <juancho637@gmail.com>
2020-11-24 08:31:51 -0500

    integración de los sockets a la aplicación

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ require("dotenv").config();
 // App Express
 const app = express();
 
+// Node Server
+const server = require("http").createServer(app);
+module.exports.io = require("socket.io")(server);
+
+// Sockets
+require("./sockets/socket");
+
 const publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT, (err) => {
+server.listen(process.env.PORT, (err) => {
   if (err) throw new Error(err);
 
   console.log("Server running on port: ", process.env.PORT);
