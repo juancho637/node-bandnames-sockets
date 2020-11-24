2182cdde75bae2583142903114dc72d47011ddf9
Juancho Garcia <juancho637@gmail.com>
2020-11-24 07:49:41 -0500

    modificación de posición de algunas lineas de código

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const app = express();
 const path = require("path");
 require("dotenv").config();
 
-const publicPath = path.resolve(__dirname, "public");
+// App Express
+const app = express();
 
+const publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
 app.listen(process.env.PORT, (err) => {
