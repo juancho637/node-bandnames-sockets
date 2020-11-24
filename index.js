d26dc3f265a3f0deccbafba3884cc828f9f10473
Juancho Garcia <juancho637@gmail.com>
2020-11-24 07:44:18 -0500

    integración de ruta path publico a la aplicación

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 require("dotenv").config();
 
+const publicPath = path.resolve(__dirname, "public");
+
+app.use(express.static(publicPath));
 
 app.listen(process.env.PORT, (err) => {
   if (err) throw new Error(err);
